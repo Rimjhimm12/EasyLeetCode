d58c4007442ffefa1013d39381d884157ec568fc
Rimjhimm12 <rimjhimmallick6@gmail.com>
2025-07-09 18:22:05 -0500

    improve method logic to avoid modulus operator
    Rewrote the method to eliminate usage of the '%' operator while maintaining the same output.
    Demonstrates that the same functionality can be achieved through different logic.

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -21,15 +21,40 @@ public class FizzBuzz {
         }
         return list;
     }
-    // Implementing new logic without using % 
 
-    public void newFizzBuzz(int n){
-        
-    }
+    // Implementing new logic without using %
+    public List<String> newFizzBuzz(int a) {
+        int fizzCounter = 0;
+        int buzzCounter = 0;
+        LinkedList<String> list = new LinkedList<String>();
+        for (int i = 1; i <= a; i++) {
+            fizzCounter++;
+            buzzCounter++;
+
+            boolean fizz = (fizzCounter == 3);
+            boolean buzz = (buzzCounter == 5);
+
+            if (fizz && buzz) {
+                list.add("FizzBuzz");
+                fizzCounter = 0;
+                buzzCounter = 0;
+            } else if (fizz) {
+                list.add("Fizz");
+                fizzCounter = 0;
+            } else if (buzz) {
+                list.add("Buzz");
+                buzzCounter = 0;
+            } else {
+                list.add(String.valueOf(i));
+                }
+            }
+            return list;
+        }
 
 
     public static void main(String[] args) {
         FizzBuzz obj = new FizzBuzz();
         System.out.println(obj.fizzBuzz(23));
+        System.out.println(obj.newFizzBuzz(23));
     }
 }
