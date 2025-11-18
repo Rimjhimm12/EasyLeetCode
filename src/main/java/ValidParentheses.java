0754a910e4f5f2dc58066f4052905dd5b9af13f8
Rimjhimm12 <rimjhimmallick6@gmail.com>
2025-11-17 23:47:38 -0600

    updated

---
+++ src/main/java/ValidParentheses.java
@@ -0,0 +1,21 @@
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.*;
+
+//use Stack ------ not yet solved
+public class ValidParentheses {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 5, 6};
+        String[] newArr = new String[arr.length];
+        for(int i = 0; i<arr.length; i++) {
+           // if (arr[i] ==) {
+
+          //  }
+        }
+
+
+        }
+
+
+    }
+
+
